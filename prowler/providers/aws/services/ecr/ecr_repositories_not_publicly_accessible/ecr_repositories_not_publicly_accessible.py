cee2a599ea587154bb7dfa44cbd627630d0d5c19
kagahd <kay.agahd@idealo.de>
2024-05-07 20:51:21 +0200

    extend check opensearch_service_domains_use_cognito_authentication_for_kibana

--- prowler/providers/aws/services/ecr/ecr_repositories_not_publicly_accessible/ecr_repositories_not_publicly_accessible.py
+++ prowler/providers/aws/services/ecr/ecr_repositories_not_publicly_accessible/ecr_repositories_not_publicly_accessible.py
@@ -14,7 +14,7 @@ class ecr_repositories_not_publicly_accessible(Check):
                 report.resource_tags = repository.tags
                 report.status = "PASS"
                 report.status_extended = (
-                    f"Repository {repository.name} is not publicly accesible."
+                    f"Repository {repository.name} is not publicly accessible."
                 )
                 if repository.policy:
                     for statement in repository.policy["Statement"]:
@@ -23,10 +23,37 @@ class ecr_repositories_not_publicly_accessible(Check):
                                 "AWS" in statement["Principal"]
                                 and "*" in statement["Principal"]["AWS"]
                             ):
+                                if "Condition" in statement:
+                                    if self.is_condition_meets_idealo_requirements(
+                                        statement["Condition"]
+                                    ):
+                                        report.status_extended = f"Repository {repository.name} is not publicly accessible because the condition of its policy meets idealo's minimum security requirements."
+                                    else:
+                                        report.status = "FAIL"
+                                        report.status_extended = f"Repository {repository.name} is publicly accessible because the condition of its policy does not meet idealo's minimum security requirements."
+                                        break
+
                                 report.status = "FAIL"
-                                report.status_extended = f"Repository {repository.name} policy may allow anonymous users to perform actions (Principal: '*')."
+                                report.status_extended = f"Repository {repository.name} is publicly accessible because its policy allows anonymous users to perform actions (Principal: '*')."
                                 break
 
                 findings.append(report)
 
         return findings
+
+    @staticmethod
+    def is_condition_meets_idealo_requirements(condition):
+
+        # Check for "aws:PrincipalOrgID" condition
+        if (
+            "StringEquals" not in condition
+            or "aws:PrincipalOrgID" not in condition["StringEquals"]
+        ):
+            return False
+
+        org_id = condition["StringEquals"]["aws:PrincipalOrgID"]
+        allowed_values = ["o-ux1i659gcz", "o-qwoiuffvxt"]
+        if isinstance(org_id, str):
+            return org_id in allowed_values
+        else:
+            return all(value in allowed_values for value in org_id)
